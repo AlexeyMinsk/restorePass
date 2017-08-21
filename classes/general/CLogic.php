88f179f3bf1910127dfd034d49ee1716b1d06c35
AlexeyMinsk <Asdrubael@tut.by>
2017-08-21 11:04:28 +0300

    second commit

--- classes/general/CLogic.php
+++ classes/general/CLogic.php
@@ -1,29 +1,74 @@
 ﻿<?php
-Class CLogic{
-	
-	public function OnBeforeUserChangePassword(){
 
-        die("in OnBeforeUserChangePassword");
+use \Bitrix\Main\Security\Random;
+
+Class CLogic
+{
+        /*$arFields = array(
+            "ID"          => 124,
+            "CONTRACT_ID" => 1,
+            "TYPE_SID"    => "LEFT"
+        );
+
+        CEvent::Send("RESTORE_PASSWORD", 's1', $arFields);*/
+
+
+    public static function OnBeforeUserSendPassword(&$arFields){
+
+        if ($arFields['LOGIN']) {
+            $rsUser = CUser::GetByLogin($arFields['LOGIN']);
+        } else {
+            $filter = Array("EMAIL" => $arFields['EMAIL']);
+            $rsUser = CUser::GetList(($by="id"), ($order="desc"), $filter);
+        }
+
+        $arUser = $rsUser->Fetch();
+        $ID = $arUser['ID'];
+
+        $randString = 'abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLNMOPQRSTUVWXYZ0123456789';
+        $passwordLenght = rand(7, 10);
+        $newPassword = Random::getStringByCharsets($passwordLenght, $randString);//randomPassword($ID);
+
+        $fields = Array(
+            "PASSWORD"          => $newPassword,
+            "CONFIRM_PASSWORD"  => $newPassword
+        );
+
+        $user = new CUser;
+        $user->Update($ID, $fields);
+
+        static::sendMail($newPassword, $arFields['SITE_ID']);
+    }
+
+    public static function sendMail($NEW_PASSW, $SITE_ID){
 
         $arFields = array(
-            "ID"          => 124,
-            "CONTRACT_ID" => 1,
-            "TYPE_SID"    => "LEFT"
+            "NEW_PASSW" => $NEW_PASSW
         );
 
-        CEvent::Send("RESTORE_PASSWORD", 's1', $arFields);
-	}
-
-	public function OnBeforeUserSendPassword(){
-		
-		//die("in OnBeforeUserSendPassword");
-		$arFields = array(
-            "ID"          => 124,
-            "CONTRACT_ID" => 1,
-            "TYPE_SID"    => "LEFT"
-        );
-
-        CEvent::Send("RESTORE_PASSWORD", 's1', $arFields);
+        CEvent::SendImmediate("RESTORE_PASSWORD", $SITE_ID, $arFields);
     }
+
+    /*public static function randomPassword($gid){
+
+        if (!is_array($gid) && is_numeric($gid) && $gid > 0) {
+            $gid = array($gid);
+        }
+
+        $policy = CUser::GetGroupPolicy($gid);
+        $length = $policy['PASSWORD_LENGTH'];
+        if ($length <= 0) {
+            $length = 6;
+        }
+
+        $alphabet = Random::ALPHABET_ALPHAUPPER
+        | Random::ALPHABET_ALPHALOWER | Random::ALPHABET_NUM;
+
+        if ($policy['PASSWORD_PUNCTUATION'] == 'Y') {
+            $alphabet |= Random::ALPHABET_SPECIAL;
+        }
+
+        return Random::getStringByAlphabet($length, $alphabet);
+    }*/
 }
 ?>
--- install/events/set_events.php
+++ install/events/set_events.php
@@ -7,65 +7,22 @@ $ERR = $et->Add(array(
     "EVENT_NAME" => "RESTORE_PASSWORD",
     "NAME" => "Изменился пароль для доступа к сайту",
     "DESCRIPTION"   => "
-        #ID# - ID баннера
-        #CONTRACT_ID# - ID контракта
-        #TYPE_SID# - ID типа
+        #NEW_PASSW# - сгенерированный пароль
         "
 ));
 
-if(!$ERR)
-	die( $et->LAST_ERROR );*/
-
 $arr["ACTIVE"] = "Y";
 $arr["EVENT_NAME"] = "RESTORE_PASSWORD";
-$arr["LID"] = 's1';//array("ru","en");//+++++++
+$arr["LID"] = 's1';//array("ru","en");
 $arr["EMAIL_FROM"] = "asdrubael@tut.by";
 $arr["EMAIL_TO"] = "rash04@tut.by";
 $arr["BCC"] = "";
-$arr["SUBJECT"] = "Тема сообщения";
-$arr["BODY_TYPE"] = "text";
+$arr["SUBJECT"] = "Пароль изменён";
+$arr["BODY_TYPE"] = "Внутри пароль ->";
 $arr["MESSAGE"] = "
-Внимание! Статус баннера # #ID# изменен.
-Тип баннера - #TYPE_SID#
-ID контракта - #CONTRACT_ID#
+Внимание! Ваш пароль изменён. Автоматичесски сгенерированный пароль - #NEW_PASSW#.
+Для изменения пароля перейдите по хешу в следующем письме.
 ";
 $eMess = new CEventMessage;
-$ERR = $eMess->Add($arr);
-
-if(!$ERR)
-	die($eMess->LAST_ERROR );
-
-/*$obEventType = new CEventType;
-$obEventType->Add(array(
-    "EVENT_NAME"    => "ADV_BANNER_STATUS_CHANGE",
-    "NAME"          => "Изменился статус баннера",
-    "LID"           => "ru",
-    "DESCRIPTION"   => "
-        #ID# - ID баннера
-        #CONTRACT_ID# - ID контракта
-        #TYPE_SID# - ID типа
-        "
-    ));
-	$arr["ACTIVE"]      = "Y";
-$arr["EVENT_NAME"]  = "ADV_BANNER_STATUS_CHANGE";
-$arr["LID"]         = array("ru","en");
-$arr["EMAIL_FROM"]  = "Asdrubael@tut.by";
-$arr["EMAIL_TO"]    = "Rash04@tut.by";
-$arr["BCC"]         = "";
-$arr["SUBJECT"]     = "Изменен статус баннера #ID#";
-$arr["BODY_TYPE"]   = "text";
-$arr["MESSAGE"]     = "
-Внимание! Статус баннера # #ID# изменен.
-Тип баннера - #TYPE_SID#
-ID контракта - #CONTRACT_ID#
-";
-
-$obTemplate = new CEventMessage;
-$obTemplate->Add($arr);
-$arFields = array(
-    "ID"          => 124,
-    "CONTRACT_ID" => 1,
-    "TYPE_SID"    => "LEFT"
-    );
-CEvent::Send("ADV_BANNER_STATUS_CHANGE", array("ru", "en"), $arFields);*/
+$ERR = $eMess->Add($arr);*/
 ?>
